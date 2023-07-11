6ddf0dc07926c644720f6257d2f2c4c264fdaa94
Mikko P <mikko.perala@tuni.fi>
2023-07-11 18:05:40 +0300

    React portfolio early working layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepagebackground">
+      <div className="header">
+        <div className="headertext">Header logo</div>
+        <ul>
+          <li><a className="secondary">Link1</a></li>
+          <li><a className="secondary">Link2</a></li>
+          <li><a className="secondary">Link3</a></li>
+        </ul>
+      </div>
+      <div className="homepage">
+        <Container className="container">
+          <Row className="content">
+            <Col className="contain-para" md={7}>
+              <h2>Hello</h2>
+              <h2>I'm Mikko Perälä</h2>
+              <a>this site fucking sucks</a>
+              <Row className="socialbuttons">
+                <button>Button1</button>
+                <button>Button2</button>
+                <button>Button3</button>
+                <button>Button4</button>
+              </Row>
+            </Col>
+            <Col className="contain-image">
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
