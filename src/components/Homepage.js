c567ba2925846770ecb42d6907d99a218dc4eb3e
Mikko <79513893+peralammikko@users.noreply.github.com>
2023-12-27 19:53:10 +0200

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -20,7 +20,7 @@ function Home () {
                 <h2 className="titletext">Hello visitor</h2>
                 <h2 className="nametext">I'm Mikko Perälä</h2>
                 <a className="welcometext">And this is my very first portfolio site</a>
-                <a className="smalltext">and still work-in-progress... :D</a>
+                <p className="smalltext">and still work-in-progress... :D</a>
                 <div className="socialbuttons">
                     <IconButton 
                         className="socialmediabutton" 
