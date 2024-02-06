218f088efc9900541ea3415293556c0c86cee922
Mikko <79513893+peralammikko@users.noreply.github.com>
2024-02-06 15:52:40 +0200

    Update Aboutme.js

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -20,7 +20,9 @@ function Aboutme() {
                     <h1 className="aboutTitle">Who is this guy anyway?</h1>
                     <p>I'm Mikko, a master's degree student in Tampere University, currently studying Human-Technology Interaction and Software Engineering. 
                         After a bachelor's degree in Material Science, I switched to Computer Science to learn more about UI/UX design related work and developing interesting software.
-                        In the summer of 2023, I started a curriculum in The Odin Project and used that knowledge to build this site!</p>
+                        As far back as I can remember, I've always wanted to be in the video games industry. Along the way, I got interested in software development in general, namely
+                        from the perspective of design & usability. I'm a creative person with an eye for details, but also someone who appreciates having a bit of fun!
+                    </p>
                 </Grid>
                 <Grid item md={5} className="aboutMeImage">
                     <CardMedia className="selfImage"
