b3bf95b57dccd0d46d85d88cfded80deaf3e2aa9
Mikko <79513893+peralammikko@users.noreply.github.com>
2024-12-20 14:35:08 +0200

    Update resume link

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,7 +12,7 @@ const Layout = () => {
                         <li><Link className="headerLink" to='/'>Home</Link></li> //
                         <li><Link className="headerLink" to='/aboutme'>About Me</Link></li> //
                         <li><Link className="headerLink" to='/projects'>Projects</Link></li>
-                        <li><Button className="resumeButton" onClick={() => window.open('https://drive.google.com/file/d/1HatZKrFRPKDRd8Xe1agaLq76a3lN2_MQ/view?usp=sharing')}>Resume</Button></li>
+                        <li><Button className="resumeButton" onClick={() => window.open('https://drive.google.com/file/d/10KVXYgcGjm4XEI4yCXs6grxEmhfD_W9F/view?usp=sharing')}>Resume</Button></li>
                     </ul>
             </div>
         </div>
