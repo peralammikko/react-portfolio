ca222c441419a26a28fff86095b20022b03e537d
Mikko <79513893+peralammikko@users.noreply.github.com>
2024-12-20 14:43:22 +0200

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -28,31 +28,37 @@ function Home () {
                     It's far from perfect, but it's a start that taught me a lot of things!
                 </h5>
                 <div className="socialbuttons">
-                    <Tooltip title="Github" placement="bottom-end">
+                    <Tooltip title="GitHub" placement="bottom-end">
                         <IconButton 
                             className="socialmediabutton" 
                             aria-label='github'
                             onClick={()=>window.open("https://github.com/peralammikko")}><GitHubIcon/>
                         </IconButton>
                     </Tooltip>
-                    <IconButton 
-                        className="socialmediabutton" 
-                        aria-label='linkedin'
-                        onClick={()=>window.open("https://www.linkedin.com/in/mikko-per%C3%A4l%C3%A4-ba092b19b/")}>
-                        <LinkedInIcon/>
-                    </IconButton>
-                    <IconButton 
-                        className="socialmediabutton" 
-                        aria-label='instagram'
-                        onClick={() => window.open("https://www.instagram.com/mikkoper/")}>
-                        <InstagramIcon/>
-                    </IconButton>
-                    <IconButton 
-                        className="socialmediabutton" 
-                        aria-label='letterboxd'
-                        onClick={()=>window.open("https://letterboxd.com/mixonlol/")}>
-                        <MovieIcon/>
-                    </IconButton>
+                    <Tooltip title="LinkedIn" placement="bottom-end">
+                        <IconButton 
+                            className="socialmediabutton" 
+                            aria-label='linkedin'
+                            onClick={()=>window.open("https://www.linkedin.com/in/mikko-per%C3%A4l%C3%A4-ba092b19b/")}>
+                            <LinkedInIcon/>
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Instagram" placement="bottom-end">
+                        <IconButton 
+                            className="socialmediabutton" 
+                            aria-label='instagram'
+                            onClick={() => window.open("https://www.instagram.com/mikkoper/")}>
+                            <InstagramIcon/>
+                        </IconButton>
+                    </Tooltip>           
+                    <Tooltip title="Letterboxd" placement="bottom-end">          
+                        <IconButton 
+                            className="socialmediabutton" 
+                            aria-label='letterboxd'
+                            onClick={()=>window.open("https://letterboxd.com/mixonlol/")}>
+                            <MovieIcon/>
+                        </IconButton>
+                    </Tooltip>
                 </div>
                 </Grid>
                 <Grid item md={5} className="homeImage">
