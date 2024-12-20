085bf369e3dc289b5f75e0b2848ae2c96525b392
Mikko <79513893+peralammikko@users.noreply.github.com>
2024-12-20 13:21:01 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,8 +9,8 @@ const Layout = () => {
             <div className="headerContainer">
                 <Link className="headerText" to='/'>mperala</Link>
                     <ul className="headerLinks">
-                        <li><Link className="headerLink" to='/'>Home</Link></li>
-                        <li><Link className="headerLink" to='/aboutme'>About me</Link></li>
+                        <li><Link className="headerLink" to='/'>Home</Link></li> //
+                        <li><Link className="headerLink" to='/aboutme'>About me</Link></li> //
                         <li><Link className="headerLink" to='/projects'>Projects</Link></li>
                         <li><Button className="resumeButton" onClick={() => window.open('https://drive.google.com/file/d/1HatZKrFRPKDRd8Xe1agaLq76a3lN2_MQ/view?usp=sharing')}>Resume</Button></li>
                     </ul>
